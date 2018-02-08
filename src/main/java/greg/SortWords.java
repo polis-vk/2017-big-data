f4e652bad8932f762c16dead493814cc4f901574
greg <greg.tuzov@gmail.com>
2018-02-09 01:44:59 +0300

    sorting  words complete

---
+++ src/main/java/greg/SortWords.java
@@ -0,0 +1,83 @@
+package greg;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparator;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import java.io.IOException;
+
+public class SortWords extends Configured implements Tool {
+
+    public static class MyMapper extends Mapper <LongWritable, Text, IntWritable, Text> {
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            final String[] data = value.toString().split("\t");
+            context.write(new IntWritable(Integer.valueOf(data[1])), new Text(data[0]));
+        }
+    }
+
+    public static class MyReducer extends Reducer <IntWritable, Text, Text, Text> {
+
+        @Override
+        protected void reduce(IntWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            super.reduce(key, values, context);
+        }
+    }
+
+    public static class Comparetor extends WritableComparator {
+
+        @Override
+        public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+            return -Integer.compare(readInt(b1, s1), readInt(b2, s2));
+        }
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+        final Job job = new Job(new Configuration(), "sort words");
+        job.setJarByClass(WordCount.class);
+
+        job.setMapperClass(MyMapper.class);
+        job.setReducerClass(MyReducer.class);
+        job.setSortComparatorClass(Comparetor.class);
+
+        TextInputFormat.addInputPath(job, new Path(args[1]));
+        TextOutputFormat.setOutputPath(job, new Path(args[1] + "_2"));
+
+        job.setInputFormatClass(TextInputFormat.class);
+        //job.setInputFormatClass(SequenceFileInputFormat.class);
+        //job.setOutputFormatClass(TextOutputFormat.class);
+        job.setMapOutputKeyClass(IntWritable.class);
+        job.setMapOutputValueClass(Text.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static void main(final String[] args) throws Exception {
+
+        if(args.length != 2) {
+            throw new IllegalArgumentException("Usage: hadoop jar <name>.jar <Class> <folder_in> <folder_out>");
+        }
+
+        final int returnCode1 = ToolRunner.run(new Configuration(), new greg.WordCount(), args);
+        final int returnCode2 = ToolRunner.run(new Configuration(), new greg.SortWords(), args);
+
+        if(0 == returnCode1 && 0 == returnCode2)
+            System.exit(0);
+        System.exit(1);
+    }
+}
--- src/main/java/greg/WordCount.java
+++ src/main/java/greg/WordCount.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -12,8 +13,10 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-public class WordCount {
+public class WordCount extends Configured implements Tool {
 
     public static class TokenizerMapper
             extends Mapper<Object, Text, Text, IntWritable>{
@@ -47,17 +50,32 @@ public class WordCount {
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    @Override
+    public int run(String[] args) throws Exception {
+
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "word count");
+
         job.setJarByClass(WordCount.class);
+
         job.setMapperClass(TokenizerMapper.class);
         job.setCombinerClass(IntSumReducer.class);
         job.setReducerClass(IntSumReducer.class);
+
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        final int returnCode = ToolRunner.run(new Configuration(), new greg.WordCount(), args);
+        System.exit(returnCode);
     }
 }
